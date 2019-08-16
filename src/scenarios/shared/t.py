23431a056137d30756ea6c1760f2786dd7c24914
Bill Wert <billwert@microsoft.com>
2019-08-16 15:32:39 -0700

    remove extra file

--- src/scenarios/shared/t.py
+++
@@ -1,7 +0,0 @@
-def m(**kwargs):
-    print(None or 'bar')
-    print('foo' in kwargs)
-    print ('%s' % kwargs['foo'] if 'foo' in kwargs else 'notfoo')
-    print ('%s' % kwargs['bar'] or 'notfoo')
-
-m(bar='blee')
