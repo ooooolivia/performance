f3ca5503728ed146071db030d2fbc2ee4eb0b294
Olivia Chen <bache@microsoft.com>
2019-09-04 12:18:39 -0700

    update cleanprojectsetup.py

--- src/scenarios/shared/cleanprojectsetup.py
+++ src/scenarios/shared/cleanprojectsetup.py
@@ -1,22 +1,23 @@
 from shutil import copytree, rmtree
-import sys
 import os
 from shared import const
+from logging import getLogger
+import sys
 
 
 # prerequisite: the asset has been copied to tmp in pre.py
 def main():
-    src = sys.argv[1]
-    tmp = os.path.join(const.TMPDIR, src)
-    assert(os.path.isdir(tmp))
-    if os.path.isdir(tmp):
-        rmtree(src)
-        copytree(tmp, src)
-    else:
-        sys.stderr.write("ERROR: Please run pre.py to copy the asset first.")
-        return 1
+    tmp = os.path.join(const.TMPDIR, const.APPDIR)
+
+    # check if a copy exists before deleting the folder
+    if not os.path.isdir(tmp):
+        getLogger().error("Please run pre.py to copy the asset first.")
+        sys.exit(1)
+
+    rmtree(const.APPDIR)
+    copytree(tmp, const.APPDIR)
 
 
 if __name__ == "__main__":
-    exit(main())
+    main()
 
