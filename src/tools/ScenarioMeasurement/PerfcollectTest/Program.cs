3f2b094f6b5b7e83ca8b6d7c6c7dc435db7e2a87
Olivia Chen <bache@microsoft.com>
2020-03-11 13:30:29 -0700

    wip

---
+++ src/tools/ScenarioMeasurement/PerfcollectTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Startup;
+using ScenarioMeasurement;
+
+namespace PerfcollectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perfcollect percollect = new Perfcollect(new Logger("name.log"));
+        }
+    }
+}
---
+++ src/tools/ScenarioMeasurement/Startup/Perfcollect.cs
@@ -0,0 +1,42 @@
+﻿using ScenarioMeasurement;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Startup
+{
+    public class Perfcollect
+    {   
+        private readonly string filepath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, @"Startup/perfcollect");
+        ProcessHelper perfcollectProcess;
+        public Perfcollect(Logger logger)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException($"Pefcollect not found at {filepath}. Please rebuild the project to download it.");
+            }
+
+            perfcollectProcess = new ProcessHelper(logger)
+            {
+                ProcessWillExit = true,
+                Executable = filepath,
+                Timeout = 300
+            };
+
+            Install();
+        }
+
+        public void StartLttng()
+        {
+
+        }
+
+        public void Install()
+        {
+            perfcollectProcess.Arguments = "install";
+            var runResult = perfcollectProcess.Run();
+        }
+    }
+}
