892b61b9ef496a63d7f696798d66c45f24e3dda5
Olivia Chen <bache@microsoft.com>
2020-04-21 20:23:53 -0700

    test Util.Windows.cs

---
+++ src/tools/ScenarioMeasurement/Util/Util.Windows.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ScenarioMeasurement
+{
+    partial class Util
+    {
+        [DllImport("User32.Dll")]
+        private static extern long SetCursorPos(int x, int y);
+
+        static partial void SetCursorPosition(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+    }
+}
